b050210f5ec624fc08c2c41b659c1abd2055652f
Deepia <30970811+Deepia@users.noreply.github.com>
2022-02-01 21:51:12 +0530

    UnitOfWork

---
+++ BulkyBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using BulkyBooks.DataAccess.Repository.IRepository;
+using BulkyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDBContext _db;
+        public CategoryRepository(ApplicationDBContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
--- BulkyBooks/Program.cs
+++ BulkyBooks/Program.cs
@@ -1,5 +1,7 @@
 
 using BulkyBooks.DataAccess;
+using BulkyBooks.DataAccess.Repository;
+using BulkyBooks.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
