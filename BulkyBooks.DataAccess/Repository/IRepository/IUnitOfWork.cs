399c19e23651ee9e32355a5a1e4daa519c31c0b9
Deepia <30970811+Deepia@users.noreply.github.com>
2022-04-09 09:02:18 +0530

    09-04-022

--- BulkyBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace BulkyBooks.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
         void Save();
 
     }
